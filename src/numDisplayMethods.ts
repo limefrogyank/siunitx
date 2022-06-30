c87ebefe93b27d6d0acc7a25b7f5a86f0e1395de
Lee McPherson <leemcpherson@hotmail.com>
2022-06-30 10:39:38 -0700

    almost done with uncertainty display and conversion

--- src/numDisplayMethods.ts
+++ src/numDisplayMethods.ts
@@ -1,5 +1,6 @@
 import { INumberPiece, IUncertainty } from "./numMethods";
 import { INumOutputOptions } from "./options";
+import { prefixSymbol } from "./units";
 
 function addSpacing(text:string, digitGroupSize:number, minimum: number, spacer:string, reverse: boolean, digitGroupFirstSize?: number, digitGroupOtherSize?: number ){
 	if (text.length >= minimum){
@@ -45,45 +46,89 @@ const groupNumbersMap = new Map<string,(num:INumberPiece, options:INumOutputOpti
 ]);
 
 
-function convertUncertaintyToPlusMinus(uncertainty: IUncertainty, piece:INumberPiece, options: INumOutputOptions) : string {
-	if (uncertainty.type == 'pm') {
-		return displayNumber(uncertainty, options);
-	} else {
-		return 'haha';
+function convertUncertaintyToPlusMinus(uncertainty: IUncertainty, piece:INumberPiece, options: INumOutputOptions) : void {
+	if (uncertainty.type != 'pm') {
+		// if there's a decimal in the uncertainty, then it's ok to display as-is
+		if (uncertainty.decimal == ''){
+			// add zeros, move whole to fraction part, and potentially add decimal and whole
+			const diff = piece.fractional.length - uncertainty.whole.length;
+			if (diff >= 0 ){
+				for (let i=0; i<diff; i++){
+					uncertainty.fractional += "0";
+				}
+				uncertainty.fractional += uncertainty.whole;
+				uncertainty.whole = '0';
+				uncertainty.decimal = options.outputDecimalMarker; 
+			} else {
+				// uncertainty is bigger than fraction.  Need to add a decimal!
+				// TO DO
+			}
+			
+		}
 	}
 }
 
-function convertUncertaintyToBracket(uncertainty: IUncertainty, piece:INumberPiece, options: INumOutputOptions) : string {
+function convertUncertaintyToBracket(uncertainty: IUncertainty, piece:INumberPiece, options: INumOutputOptions) : void {
 	if (uncertainty.type == 'bracket') {
-		return 'poop';
+		// check to make sure that uncertainty doesn't need a decimal point via 'compact marker'
+		const diff = uncertainty.whole.length - piece.fractional.length;
+		if (diff > 0 && options.uncertaintyMode == 'compact-marker'){
+			uncertainty.fractional = uncertainty.whole.slice(diff, uncertainty.whole.length);
+			uncertainty.whole = uncertainty.whole.slice(0, diff);
+			uncertainty.decimal = options.outputDecimalMarker;
+		}
+		
 	} else {
-		return 'pee';
+		//easiest way is to convert to a number and check if less than zero
+		const strNum = uncertainty.whole + uncertainty.decimal + uncertainty.fractional;
+		const num = +(strNum);
+		// if less than 1 (just a fraction), then remove leading zeros.  Else leave it as is.
+		if (num < 1) {
+			let position=0;
+			for (let i=0; i<uncertainty.fractional.length;i++){
+				if (uncertainty.fractional[i] != '0'){
+					break;
+				}
+				position++;
+			}
+			uncertainty.whole = uncertainty.fractional.slice(position, uncertainty.fractional.length);
+			uncertainty.decimal = '';
+			uncertainty.fractional = '';
+		}
 	}
+}
+
+function displayUncertaintyBracket(uncertainty: IUncertainty, options: INumOutputOptions):string{
+	let output = options.uncertaintySeparator;
+	output += options.outputOpenUncertainty;
+	output += uncertainty.whole;
+	output += options.uncertaintyMode == 'compact-marker' && uncertainty.decimal != '' ? options.outputDecimalMarker : '';
+	output += uncertainty.fractional; 
+	output += options.outputCloseUncertainty;
+	return output;
+}
+
+function displayUncertaintyPlusMinus(uncertainty: IUncertainty, options: INumOutputOptions):string{
+	let output = '\\pm';
+	return output + displayNumber(uncertainty, options);
 }
 
 const uncertaintyModeMapping = new Map<string, ( uncertainty:IUncertainty, value: INumberPiece, options: INumOutputOptions)=>string>([
 	['separate', (uncertainty: IUncertainty, value:INumberPiece, options: INumOutputOptions):string => {
-		let output = '\\pm';
-		output += convertUncertaintyToPlusMinus(uncertainty, value, options);
-		return output;
+		convertUncertaintyToPlusMinus(uncertainty, value, options);
+		return displayUncertaintyPlusMinus(uncertainty, options);
 	}],
 	['compact', (uncertainty:IUncertainty,value: INumberPiece,  options: INumOutputOptions):string => {
-		let output = options.outputOpenUncertainty;
-		output += convertUncertaintyToBracket(uncertainty, value, options);
-		output += options.outputCloseUncertainty;
-		return output;
+		convertUncertaintyToBracket(uncertainty, value, options);
+		return displayUncertaintyBracket(uncertainty, options);
 	}],
 	['full', (uncertainty:IUncertainty,value: INumberPiece, options: INumOutputOptions):string => {
-		let output = options.outputOpenUncertainty;
-		output += convertUncertaintyToBracket(uncertainty, value, options);
-		output += options.outputCloseUncertainty;
-		return output;
+		convertUncertaintyToBracket(uncertainty, value, options);
+		return displayUncertaintyBracket(uncertainty, options);
 	}],
 	['compact-marker', (uncertainty:IUncertainty,value: INumberPiece,  options: INumOutputOptions):string => {
-		let output = options.outputOpenUncertainty;
-		output += convertUncertaintyToBracket(uncertainty, value, options);
-		output += options.outputCloseUncertainty;
-		return output;
+		convertUncertaintyToBracket(uncertainty, value, options);
+		return displayUncertaintyBracket(uncertainty, options);
 	}],
 ])
 
@@ -113,8 +158,13 @@ export function displayOutput(num:INumberPiece, options: INumOutputOptions):stri
 	//const piece = pieces.find((v)=> v.type == 'number');
 	let output = '';
 	
+	// display any prefix symbol such as less than, greater than, etc.
+	output += num.prefix;
+	
+	// display main number
 	output += displayNumber(num, options);
-		
+	
+	// display uncertanties
 	num.uncertainty.forEach(v=>{
 		output += uncertaintyModeMapping.get(options.uncertaintyMode)(v,num,options);
 	});
